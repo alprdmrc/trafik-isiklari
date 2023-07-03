89e087b55228c594e5ce35fa41d4693dc5923d55
alperdemirci <alprdmrc20@gmail.com>
2023-07-03 15:59:57 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -7,22 +7,49 @@ const lamps = ["gr1", "gr2", "gryaya"];
 function App() {
   const [activeLamp, setActiveLamp] = useState("wait1");
   const [greenTime, setGreenTime] = useState(5000);
+  const [step, setStep] = useState(0);
+  const [timer, setTimer] = useState(5);
   const interval = 5000;
   var activeTime = interval + greenTime;
 
   const handleYaya = (e) => {
     e.preventDefault();
     setActiveLamp("gryaya");
+    setTimer(15);
   };
 
   const handleGreenTime = (e) => {
-    setGreenTime(e.target.value * 1000);
+    let greenInput = e.target.value;
+    if (greenInput > 30) {
+      greenInput = 30;
+    } else if (greenInput < 1) {
+      greenInput = 1;
+    }
+    setGreenTime(greenInput * 1000);
+  };
+
+  const nextStep = () => {
+    step < 6 ? setStep(step + 1) : setStep(1);
+
+    if (step === 1 || step === 3) {
+      setTimer(greenTime / 1000);
+    } else if (activeLamp === "gryaya") {
+      setTimer(15);
+    } else {
+      setTimer(5);
+    }
   };
 
   useEffect(() => {
     let timeout = null;
+
+    let countdown = setInterval(() => {
+      setTimer((time) => time - 1);
+    }, 1000);
+
     switch (activeLamp) {
       case "wait1":
+        setStep(1);
         timeout = setTimeout(function () {
           setActiveLamp("gr1");
         }, interval);
@@ -43,6 +70,7 @@ function App() {
         }, interval);
         break;
       case "gryaya":
+        setTimer(15);
         timeout = setTimeout(function () {
           setActiveLamp("wait1");
         }, 15000);
@@ -50,29 +78,44 @@ function App() {
       default:
         console.log("unknown state");
     }
-    if (timeout) {
-      return () => clearTimeout(timeout);
-    }
+
+    return () => {
+      clearTimeout(timeout);
+      clearInterval(countdown);
+    };
   }, [activeLamp]);
 
   return (
     <div className="App">
-      {lamps.map((lamp, index) => (
-        <TrafficLamp
-          greenTime={greenTime}
-          activeLamp={activeLamp}
-          key={index}
-          id={lamp}
+      <div className="panel">
+        <p>Mevcut Adim : {activeLamp === "gryaya" ? "Yaya Gecisi" : step}</p>
+        <p>Sonraki Adima Kalan Sure : {timer}</p>
+      </div>
+      <div className="cadde">
+        {lamps.map((lamp, index) => (
+          <TrafficLamp
+            greenTime={greenTime}
+            activeLamp={activeLamp}
+            nextStep={nextStep}
+            key={index}
+            id={lamp}
+            setTimer={setTimer}
+          />
+        ))}
+      </div>
+      <div className="controller">
+        <button className="yayabutonu" onClick={handleYaya}>
+          Yaya
+        </button>
+        <input
+          onChange={handleGreenTime}
+          className="yesilInput"
+          type="number"
+          min="1"
+          max="30"
+          value={greenTime / 1000}
         />
-      ))}
-      <button className="yayabutonu" onClick={handleYaya}></button>
-      <input
-        onChange={handleGreenTime}
-        className="yesilInput"
-        type="number"
-        min="1"
-        max="30"
-      />
+      </div>
     </div>
   );
 }
